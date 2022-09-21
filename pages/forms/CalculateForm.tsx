7843a91ac95c58f39dee4a21671b0200835aa275
Ola Taube <ola@tessin.com>
2022-09-21 20:54:51 +0200

    Fixed money formatting.

--- pages/forms/CalculateForm.tsx
+++ pages/forms/CalculateForm.tsx
@@ -4,51 +4,14 @@ import { Button, Card, CardContent, TextField } from "@mui/material";
 import Stack from "@mui/system/Stack/Stack";
 
 import { NumericFormat } from "react-number-format";
-
-function NumberFormatCustom(props) {
-  const { inputRef, onChange, ...other } = props;
-
-  return (
-    <NumericFormat
-      {...other}
-      getInputRef={inputRef}
-      onValueChange={(values) => {
-        onChange({
-          target: {
-            name: props.name,
-            value: values.value,
-          },
-        });
-      }}
-      thousandSeparator
-      isNumericString
-      decimalScale={0}
-    />
-  );
-}
+import { FormattedInput } from "./TextFieldMoney";
 
 export const CalculateForm = () => {
-  const [loan, setLoan] = React.useState(4000000);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const x = event.target.value;
-    setLoan(parseInt(x));
-  };
-
   return (
     <Card>
       <CardContent>
         <Stack spacing={3}>
-          <TextField
-            label="Lån"
-            value={loan.toFixed(2)}
-            onChange={handleChange}
-            name="loan"
-            id="loan"
-            InputProps={{
-              inputComponent: NumberFormatCustom,
-            }}
-          />
+          <FormattedInput value="4000000" />
           <Button variant="contained">Calculate</Button>
         </Stack>
       </CardContent>
